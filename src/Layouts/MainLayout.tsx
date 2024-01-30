e514bc8a27f18d7ffaba37584f1ea72bdaf481d6
Pol Gubau <gubaupol@gmail.com>
2024-01-30 08:40:06 +0100

    Update MainLayout and SudokuLobby components

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -30,20 +30,25 @@ _____________________________________________________
 `);
   return (
     <main className="bg-primary md:p-8 w-screen h-[100svh] grid grid-rows-[auto,1fr] gap-6 overflow-hidden">
-      <nav className="bg-secondary-50 w-full p-2 md:rounded-full justify-start gap-2 md:gap-4 flex items-center z-10">
-        <div className="flex items-center bg-primary-900 h-full aspect-square rounded-full text-secondary-50 justify-center text-3xl md:p-4">
-          <IoGameController />
-        </div>
+      <nav className="bg-secondary-50 w-full p-3 md:rounded-full justify-start gap-2 md:gap-4 flex items-center z-10">
+        <Link to="/" title="Go to main screen">
+          <div className="flex items-center bg-primary-900 h-full p-1 aspect-square rounded-full text-secondary-50 justify-center text-3xl md:p-4">
+            <IoGameController className="text-xl md:text-4xl" />
+          </div>
+        </Link>
         <hgroup className="text-lg md:text-4xl flex gap-1 items-center">
           <Link
             to="https://polgubau.com"
             target="_blank"
             rel="noopener noreferrer"
+            title="Check my portfolio"
           >
             <h2>Pol/</h2>
           </Link>
           <h1 className="font-bold">
-            <Link to="/">{title ?? "Games"}</Link>
+            <Link to="/" title="Go to main screen">
+              {title ?? "Games"}
+            </Link>
           </h1>
         </hgroup>
       </nav>
--- src/screens/sudoku/screens/sudoku-lobby.tsx
+++ src/screens/sudoku/screens/sudoku-lobby.tsx
@@ -10,10 +10,11 @@ interface Props {
 const SudokuLobby: React.FC<Props> = ({ onStart, setDifficulty }: Props) => {
   return (
     <div className="p-4 pt-8 flex flex-col gap-8 items-center h-auto justify-between md:justify-center ">
-      <h3 className="text-2xl md:text-5xl max-w-[600px] text-center font-bold ">
-        Are you ready to beat the sudoku? 🧐
-      </h3>
+      <div></div>
       <div className="flex flex-col gap-2 justify-center w-full max-w-[500px] text-center">
+        <h3 className="text-3xl md:text-5xl max-w-[600px]  font-bold">
+          Are you ready to beat the sudoku?🧐
+        </h3>
         <Label htmlFor="difficulty" className="text-lg md:text-2xl">
           How hard do you want it to be? 😈
         </Label>
@@ -30,7 +31,6 @@ const SudokuLobby: React.FC<Props> = ({ onStart, setDifficulty }: Props) => {
           className="w-full max-w-[500px]"
         />
       </div>
-
       <p className="flex gap-2 w-full max-w-[600px]">
         <Tooltip content="Return home (the game won't be saved)">
           <motion.div
